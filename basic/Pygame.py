e61f727463463bc75722acd1c6a3ba13a6eaf0d3
axrain77 <cvb3177@gmail.com>
2026-05-20 18:11:32 +0800

    Create Pygame.py

---
+++ basic/Pygame.py
@@ -0,0 +1,13 @@
+#keys
+-Use Pygame to create a graphical game window
+-Handle user input to control the game
+-Create and update game objects
+-Detect collisions in a game
+-Display and update the game score
+
+##Create a new file named snake_game.py
+touch snake_game.py
+##creating a blank file named snake_game.py
+sudo pip install pygame
+##install pygame(pre-written code)
+
