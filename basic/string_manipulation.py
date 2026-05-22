a840743a27f383f6d6cca0b87ee5caf6604acb7c
axrain77 <cvb3177@gmail.com>
2026-05-22 22:57:43 +0800

    Update string_manipulation.py

--- basic/string_manipulation.py
+++ basic/string_manipulation.py
@@ -1,1 +1,18 @@
+##create a script that processes an astronaut's name to create a standardized name tag.
 
+#Create a Python script file
+touch name_tag_processor.py
+
+#ask user for his name
+full_name = input("What is your full name?")
+
+#convert name into uppercase
+uppercase_name = full_name.upper()
+
+#replace any space in the name to underscore
+name_with_underscores = uppercase_name.replace(" ", "_")
+
+#refer to screenshot pss18 in readme.md for result
+print("Enter astronaut's full name:",full_name)
+print("Original name:",full_name)
+print(f"Processed name tag: ASTRONAUT_{name_with_underscores}")
