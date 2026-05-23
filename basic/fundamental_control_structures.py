3827e2747593e0d7637d2cd920388c97bf8008cd
axrain77 <cvb3177@gmail.com>
2026-05-23 21:54:04 +0800

    Create fundamental_control_structures.py

---
+++ basic/fundamental_control_structures.py
@@ -0,0 +1,61 @@
+##this file will include conditional statements and loops exercises.
+
+#exploring if statement
+age = 20
+#refer to screenshot pss19 in readme.md for result
+if age >= 18:
+    print("You are an adult.")
+
+#adding else clause in if statement. result will be either one of the outputs condition i indicated.
+age = 15
+#refer to screenshot pss20 in readme.md for result
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+#adding elseif clause in if statement. use it if you have multiple conditions and result.
+age = 65
+#refer to screenshot pss21 in readme.md for result
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+##Exploring "For" Loops
+#simple for loop using range.
+#refer to screenshot pss22 in readme.md for result
+for i in range(5):
+    print(i)
+
+##exploring multiple arugments in range(), start, end, and step values.
+#generates a sequence of numbers starting from 1, up to (but not including) 10, with a step of 2.
+#refer to screenshot pss23 in readme.md for result
+for i in range(1, 10, 2):
+    print(i)
+
+#for loop in list
+fruits = ["apple", "banana", "cherry"]
+#each round the fruit variable will take a value from the list fruits starting from first one which is apple.
+for fruit in fruits:
+    print(f"I like {fruit}")#refer to screenshot pss24 in readme.md for result
+
+#using for loop with strings, each round char variable will take a letter from the word Python and convert in uppercase
+#refer to screenshot pss25 in readme.md for result
+for char in "Python":
+    print(char.upper())
+
+#combine a "for" loop with conditional statements. % is a modulo operator,returns the remainder or signed remainder of a division. eg 10 % 2 is 0 because 2 goes into 10 5 times with 0 remainder left. 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#refer to screenshot pss26 in readme.md for result
+for num in numbers:
+    if num % 2 == 0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+
+  
